2fe5826f7a5d8ccfdf78bfac1b3b65ac10a40fa4
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-07 15:12:24 +0530

    20426

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -1,42 +1,10 @@
-import os
-import django
-from django.conf import settings
 from django.db import models
-from django.core.mangement import call_command
+from django.http import HttpResponse
+from django.urls import path
+from django.template import Template, Context
+from django.shortcuts import render
 
-
-# step1 : configure Django setting inline
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-setting.configure(
-    DEBUG=True,
-    ROOT_URLCONF=__name__,
-    ALLOWED_HOST=[],
-    SECRET_KEY = 'insecure-secret-key'
-    INSTALLED_APP=[
-        'django.contrib.contenttype',
-        'django.contrib.auth',
-        'menuapp',
-    ],
-    DATABASE={
-        'default':{
-            'ENGINE':'django.db.backends.squite3',
-            'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
-        }
-    },
-)
-#Step 2 : Setup Django 
-django.setup()
-
-#step3 : Define a fake app config so Django seesnit
-from djano.apps import AppConfig, apps
-class MenuAppConfig(AppConfig):
-    name = 'menuapp'
-    label = 'menuapp'
-
-if not apps.ready:
-    apps.populate(setting.INSTALLED_APPS + ['menuapp'])
-
-#step 4 Define the model
+#Models
 class MenuItem(models.Model):
     name = models.CharField(max_lenght = 100)
     description = models.textField
@@ -45,19 +13,39 @@ class MenuItem(models.Model):
     def__str__(self):
         return self.name
 
-    class Meta:
-        app_label = 'menuapp'
+#View
+def Menu_view(request):
+    items = MenuItem.object.all()
 
-# Step 5 : Create and apply migrations
-if__name__ == "__main__":
-    # Make migration file and apply them 
-    from django.core.mangement.commands import makemigrations, migrate
+    #HTML Template as string
+    html_template = """
+    <!DOCTPYE html>
+    <html>
+    <head>
+        <title>Our Menu</title>
+    </head>
+    <body>
+        <h1>Menu</h1>
+        <ul>
+        {% for item in items %}
+            <li>
+                <h2>{{ item.name }}</h2>
+                <p>{{ item.description }}</p>
+                <strong>${{ item.price }}</strong>
+            </li>
+        {% empty %}
+            <li> NO menu items  available </li>
+        {% endfor %}
+        </ul>
+    </body>
+    </html>
+    """
 
-    # Create migration 
-    call_command("makemigrations","menuapp", verbosity=1)
-    #Apply mrigration
-    call_command("migrate", verbosity=1)
+    template = Template(html_template)
+    context =Context({'item': item})
+    return HttpResponse(template.render(context))
 
-    #Optional: create and save a sample item
-    item = MenuItem(name="pizza", description="Chessy delicious pizza", price= 9.99)
-    item.save
+#URL
+urlpatterns =[
+    path('menu/', men_view),
+]
