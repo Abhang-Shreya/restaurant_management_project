7db4de2b7006f59628ab810e4da48b47d9dd0c3a
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-08 13:10:53 +0530

    20427

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -1,92 +1,43 @@
-from django.db import models
-from django import from
-from django.shortcuts import render , redirect
-from django.conf import settings
-from django.conf.urls.statics import statics
-from django.urls import path 
+#app.py (or view.py in your Django app)
+ from django.db import models
+ from django.http import HttpResponse
+ from django.urls import path
+ from django.apps import AppConfig 
+ from django.core.wsgi import get_wsgi_application
 
-#Models
-class MenuItem(models.Model):
-    name = models.CharField(max_lenght = 100)
-    description = models.textField
-    price = models.DecimalField(max_dight=6, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/',blank=True, null= True)
+ #Model
+ class RestaurantInfo(models.Model):
+    name = models.CharField(max_lenght=100)
 
     def__str__(self):
         return self.name
 
-#View
-def Menu_view(request):
-    if request.method == 'POST':
-        form = MenuItemForm(request.post, request.files)
-        if form.is_valid():
-            form.save()
-            return redirect('menu')
-        else:
-            form = MenuItemForm()
-        return render(request, 'add_menu_item.html',{'form':form})
-    
-    def Menu_view(request):
-        item = MenuItem.objects.all()
-        return render(request, 'menu.html',{'item':items})
+#view 
+def homepage(request):
+    restaurant = RestaurantInfo.objects.first()
+    name = restaurant.name if restaurant else "My Restaurant"
+    return HttpResponse(f"""
+        <html>
+        <head>
+        <title>
+            {name}
+        </title>
+        </head>
+        <body>
+            <h1>Welcome to{name}</h1>
+        </body>
+        </html>
+    """)
 
-#Template (for reference; put in template folder)
-#add_menu_item.html
- """
-<!DOCTPYE html>
-<html>
-<head>
-<title>Add Menu Item </title>
-</head>
-<body>
-    <h2>Add Menu Item </h2>
-    <form method = "post" enctype="multipart/form-data>
-        {% csrf_token %}
-        {{ form.as_p }}
-        <button type="submit">Save</buttom>
-    </form>
-    <a href="{% url 'menu' %}">View Menu </a>    
-</body>
-</html>
-"""
+#App config
+class OnefileAppConfig(AppConfig):
+    name = 'omefileapp'
+    verbose_name = "One File App"
 
-#menu.html
-"""
-<!DOCTPYE html>
-<html>
-<head>
-<title>Menu</title>
-</head>
-<body>
-    <h2>Menu</h2>
-    <ul>
-        {% for item in items %}
-            <li>
-                <strong>{{  item.name }}</strong> - ${{ item.price }}</br>
-                {{ item.image }}
-                    <img src="{{ item.image.url }}"width="150" />
-                {% endif %}
-            </li>
-        {% endfor %}
-    </ul>
-    <a herf = {% url 'add_item' %}>Add New Menu</a>
-</body>
-</html>
-"""
-
-#URL
-urlpatterns =[
-    path('add', add_menu_item, name='add_item'),
-    path('menu/', menu_view, name='menu')
+#URLS
+urlpatterns = [
+    path('', homepage),
 ]
 
-#SETTING{Usually in setting.py}
-BASE_DIR = os.path.join(os.path.abspath(__file__))
-
-MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
-DEBUG = True
-
-if DEBUG:
-    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+#(optional) Include this for WSGI if deploying in a single file setup
+application = get_wsgi_application()
