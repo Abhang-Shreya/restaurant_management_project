d43f2c15efdbff24a78367196fe04c49ce5b0de9
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-14 11:48:42 +0530

    31958

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -1,69 +1,63 @@
-# contact_page.py
-
-from django.http import HttpRespone
-from django.urls import path
-
-def contact_us(request): 
-    html_content = """   
-    <!DOCTYPE html>
-    <html="en">
-    <head>
-        <meta charest="UTF-8">
-        <title>
-            Contact Us
-        </title>
-        <style>
-            body{
-                font-family: Arial, sans-serif;
-                background-color: #f9f9f9;
-                margin: 0;
-                padding: 0;
+<!DOCTYPE html>
+<html="en">
+<head>
+    <meta charest="UTF-8">
+    <meta name="viewport" contact="width=dievice-width, intial-scale=1.0">
+    <title>
+        Reservations
+    </title>
+    <style>
+        body{
+            font-family: Arial, sans-serif;
+            background-color: #f9f9f9;
+            color: #333;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-driection: coloum;
+            min-hight: 100vh;
             }
 
-            .container {
-                max-width: 600px;
-                margin: 50px auto;
-                background-color: white
-                padding: 20px;
-                border-radius: 8px;
-                box-shadow: 0 0 10px rgba(0,0,0,0.1);
+            header {
+                background-color: #444;
+                color: #fff;
+                padding: 15px;
                 text-align: center;
             }
 
-            h1 {
-                margin-top: center;
-                color: #333; 
+            main {
+                flex: 1;
+                display: flex;
+                algin-item: center;
+                justify-content: center;
             }
 
-            .info {
-                margin-top: 20px;
+            .placeholder {
+                font-size: 1.5rem;
+                text-align: center;
+                padding:20px;
+                background-color: #fff;
+                border-radius: 8px;
+                box-shadow: 0px 2px 8px rgba(0,0,0,0.1);
             }
 
-            .info p {
-                margin: 8px 0;
-                font-size: 16px;
-                color: #555;
+            footer {
+                background-color: #444;
+                color: #fff;
+                padding: 10px;
+                text-align: center;
             }
-
-            .info strong{
-                color: #000;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Contact Us</h1>
-            <div class="info">
-                <p><strong>Address:</strong>123 Food street, pune, India<p>
-                <p><strong>Phone:</strong> +91 98765 43210</p>
-                <p><strong>Email:</strong>contact@myrestaurant.com</p>
-            </div>
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Reservations</h1>
+    </header>
+    <main>
+        <div class="placeholder">
+            Our online reservation system will be available soon<br>
+            Please check back later!
         </div>
-    </body>
-    </html>
-    """
-    return HttpRespone(html_content)
-
-urlpatterns = [
-    path('contact/', contact_us)
-]
+    </main>    
+</body>
+</html>
