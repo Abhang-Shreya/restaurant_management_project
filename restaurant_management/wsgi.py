66f33e8122c3dab4b26632d423a6f582129e9cbb
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-18 14:53:39 +0530

    20432

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -1,25 +1,38 @@
+from django.db import models
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import path
 from django.core.management import execute_from_command_line
 import sys
 
+class RestaurantLocation(models.Model):
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+
+    def__str__(self):
+        return f"{self.address},{self.city},{self.state}{self.zip_code}"
+
 def home (request):
     try:
-        # Example risky opration (like a database query)
-        #For now, Let's simulate a possible error 
-        result = 10 /0 # This will raise ZeroDivisionError
-        html = f"<h1>Welcome!</h1><p>Result: {reuslt}</p>"
-
-    except ZeroDivisionError:
-        html ="""
-        </h1>Oops!</h1>
-        <p>Someting went wrong while processing your request (division by zero).</p>
-        <p>Please try agaon late.</p>
-        """
+        location = RestaurantLocation.objects.first()
+        if location:
+            html = f"""
+            <h1>Restaurant Location</h1>
+            <p><strong>Address: </strong>{location.address}</p>
+            <p><strong>City: </strong>{location.city}</p>
+            <p><strong>Status: </strong>{location.state}</p>
+            <p><strong>Zip Code: </strong>{location.zip_code}</p>
+            """
+        else:
+            html ="
+            </h1>No location details found. please add in the database </h1>
+            "
 
     excepet Excepetion as e :
         html f"""
-        <h1>Unexpect Error</h1>
+        <h1> Error</h1>
         <p>{str(e)}</p>
         """
 
@@ -37,6 +50,12 @@ setting.configure(
     SECRET_KEY="secret",
     ROOT_URLCONF=__name__,
     ALLOWED_HOST=["*"],
+    INSTALLED_APPS=[
+        "django.contrib.contenttypes",
+        "django.contrib.auth",
+        __name__
+    ],
+    DATABASES={"default":{"ENGINE":"django.db.backends.sqlite3","NAME":"db.sqlite"}},
 )
 
 #Main
