a38394d07678018b57f872355e04bb73a81358af
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-18 23:22:53 +0530

    35102

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -20,7 +20,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
-    def __str__(self):
+    def __str__(self):  
         return self.name
 
 def home (request):
