2f1a1b189e66d84cacd0efe4c1a38e81f0d80275
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-20 19:40:19 +0530

    36100

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -1,86 +1,40 @@
-from django.shortcuts import render
-from django.urls import path
+from django.http import HttpResponse
+from django.utils import timezone
 
-#Views
 def home(request):
-    breadcrumb =[("home", "/")]
-    return render_page(request, "home.html",{"breadcrumbs": breadcrumbs})
+    #Get current date and time 
+    now = timezone.now()
+    formatted_now = now.strftimr("%A %d %B %Y, %I:%M %P")
 
-def about(request):
-    breadcrumb = [("Home", "/")("about","/about/")]
-    return render_page(request, "about.html",{"breadcrumbs": breadcrumbs})
-
-def contact(request):
-    breadcrumb = [("Home", "/"), ("Contact", "/contact/")]
-    return render_page(request, "contact.html",{"breadcrumbs": breadcrumbs})
-
-#URL Patterns
-urlpatterns =[
-    path("", home, name="home"),
-    path("about/", about, name="about"),
-    path("contact", contact, name="contact"),
-]
-
-#base.html 
-"""
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Restaurant Site</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
+    html = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Restaurant Homepage </title>
+        <style>
+        body {{ 
+            font-family:Arial, sans-serif; 
             margin: 20px;
-        }
-        nav.breadcrumbs{
-            font-size: 14px;
-            margin-bottom: 15px;
-        }
-        nav.breadcrumbs a{
-            text-decoration: none;
-            color: #555;
-        }
-    </style>
-</head>
-<body>
-    <nav class="bradcrumbs">
-        {% for name, url in bradcrumbs %}
-            {% if not forloop.last %}
-                <a herf="{{ url }}">{{ name }}</a><span></span>
-            {% else %}
-                <span>{{ name }}</sapn>
-            {% endif %}
-        {% endfor %}
-    </nav>
-
-    {% block content %}{% endblock %}
-</body>
-</html>
-"""
-
-#home.html
-"""
-{%extends "base.html" %}
-{% block content %}
-<h1>Welcome to Our Restaurant</h1>
-<p>Enjoy delicious meals with us.</p>
-{% endblock %}
-"""
-
-# about.html
-"""
-{% extends "base.html" %}
-{% block contact %}
-<h1>About Us<h1>
-<p>WE have been serving food since 1990...</p>
-{% endblock %}
-"""
-
-# contact.html
-"""
-{% extends "base.html" %}
-{% block content %}
-<h1>Contact Us </h1>
-<p>Email: info@restaurant.com</p>
-{% endblock %}
-"""
+            }}
+        .datetime-box {{
+            background: #f8f8f8;
+            padding: 10px 20px;
+            box-shadow: 0px 2px 5px rgba(0,0,0,0.1);
+            display: inline-block;
+            }}
+            h2{{
+                color:#333;
+            }}
+        </style>
+    </head>
+    <body>
+        <h1>Welcome to Our Restaurant</h1>
+        
+        <div class="datetime-box">
+            <h2>Current Date & Time</h2>
+            <p>{formatted_now}</p>
+        <div>
+    </body>
+    <html>
+    """
+    return HttpResponse(html)
