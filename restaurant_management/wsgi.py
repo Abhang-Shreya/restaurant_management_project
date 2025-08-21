051512887371d68ece0fc654068a1ccad85f3d21
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-21 20:23:44 +0530

    36585

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -2,50 +2,96 @@
 from django.http import HttpResponse
 from django.urls import path, reverse
 import random
+from django.urls import path
+from . import views
 
-#VIEW
-def order_confirmation(request):
-    order_number = random.randint(1000,9999)
-    html = f"""
+#Inline template 
+template_str ="""
     <!DOCTYPE html>
-    <html lang="en">
+    <html>
     <head>
-        <meta charset="UTF_8">
-        <title>Order Order Confirmation</tile>
+        <title>Restaurant Homepage</tile>
         <style>
-            body {{
+            body {
                 font-family: Arial, san-serif;
-                text-align: center;
-                margin-top: 80px;
-            }}
-            .confirmation-box {{
-                display: inline-block;
-                padding: 20px 40px;
-                border: 2px solid #4CAF50;
-                border-radius: 10px;
-                background: #f9fff9;
-                box-shadow: 0 0 10px rgba(0,0,0,0.1);
-            }}
-            h1{{
-                color: #4CAF50
-            }}
-            p {{
-                font-size: 18px;
-            }}
+                padding: 30px;
+            }
+            .container {
+                max-width: 400px; margin: auto;
+            }
+            .errorlist{
+                color: red;
+                list-style: none;
+                padding: 0;
+            }
+            .success {
+                color:green;
+            }
+            from {
+                display: flex;
+                flex-direction: column;
+            }
+            input {
+                margin: 5px 0;
+                padding: 8px;
+            }
+            button {
+                margin-top: 10px;
+                padding: 10px; 
+                background: #4CAF50;
+                color: white;
+                border: none;
+            }
+            buuton:hover {
+                background: #45a049;
+                cursor: pointer;
+            }
         </style>
     </head>
     <body>
-        <div class="confirmation-box">
-            <h1>✅ Order Confirmed</h1>
-            <p>THANK YOU FOE YOUR PURCHASE.</p>
-            <p>Your order number is : <strong>#{order_number}</strong></p>
+        <div class="container">
+            <h1>Welcome to Our Restaurant</h1>
+
+            {% if user.is_authenicated %}
+                <p class="success">Hello, {{ user.username }}! You are logged in.</p>
+                <form method="post" action="{% uel 'logout' %}">
+                    {% csrf_token %}
+                    <button type="submit">Logout</button>
+                </form>
+            {% else %}
+                <h2>Login</h2>
+                <form method="post">
+                    {% csrf_token %}
+                    {{ form.as_p }}
+                    <button type="submit">Login</button>
+                </form>
+            {% endif %}
         </div>
     </body>
     </html>
     """
-    return HttpResponse(html)
+
+    django_engine = engines['django']
+    template = django_engine.form_string(template_str)
+
+    def home(request):
+        if request.method == "POST":
+            form = AuthenicationForm(request, data=request.POST)
+            if form.is_valid():
+                user = form.get_user()
+                login(request, user)
+                return redirect("home")
+        else:
+            form = AuthenicationForm()
+    return HttpResponse(template.render({"form": form}, request))
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("home")
 
 # URLS
 urlpatterns = [
-    path("order/confirmation/", order_confirmation, name="order_confirmation"),
+    path("", view.home, name="home"),
+    path("logout/", view.logout_view, name="logout"),
 ]
