00ffd5fc06c83dd12d4172e4c84bb95257d26f85
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-23 21:32:59 +0530

    37770

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -1,84 +1,112 @@
-# restaurant_app
-from djano.core.paginator import paginator
-from django.shortcuts import render
+# restaurant_app.py
 from django.db import models
+from django.shortcuts import render
+from django.urls import path 
+from django.http import HttpResponse
 
 #Model
-class MenuItem(models.Model):
+class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    image = models.ImageField(upload_to='restaurant/')
+
+class MenuItem(models.Model):
+    name = models.Charfield(max_length=100)
+    description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu/')
 
-    def __str__(self):
-        return self.name
-
+class Special(models.Model):
+    name = models.CharField(max_length=100)
+    description = models>TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = model.ImageField(upload_to='specials/')
+    
 #View
+def homepage(request):
+    special = Special.object.all()
+    menu_items = MenuIetms.object.all()[:5]#just a few 
+    return render(request, "homepage.html", {"special": special, "menu_items": menu_items})
+
+def about(request):
+    restaurant = Restaurant.objects.first()
+    return render(request, "about.html", {"restaurant": restaurant})
+
 def menu(request):
-    item_list = MenuItem.objects.all().order_by("id")#fetch all items
-
-    paginator = Paginator(item_list, 5) # 5 items per page
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    return render(request, "menu.html", {"page_obj": page_obj})
+    item_list = MenuItem.objects.all()
+    return render(request, "menu.html", {"items": items})
 
 #urls.py
-from django.urls import path
-from . import views 
 
 urlpatterns = [
-    path("menu/", views.menu, name="menu"),
+    path('', homepage, name="homepage"),
+    path('about/' about, name="about"),
+    path("menu/", menu, name="menu"),
 ]
 
-#Template/menu.html
+#Template
+#homepage.html
 """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Menu</title>
-    <style>
-        .pagination {
-            margin: 40px;
-            display: flex;
-            gap: 10px;
-        }
-        .paginationn a, .paginationation span  {
-            padding: 25px;
-            border: 1px solid #ddd;
-            text-decortion: none;
-        }
-        .pagination .current {
-            background: #007BFF;
-            color: white;
-        }
-    </style>
+    <title>Restaurant Homepage </title>
 </head>
 <body>
-    <h1>Menu</h1>
-    {% for item in page_obj %}
+    <h1>Restaurant Homepage </h1>
+    {% for special in specials %}
         <div>
-            <h3>{{ item.name }} - ${{ item.price }}</h3>
-            <p>{{ item.decrition }}</p>
+        <img src="{{ special.image.url }}" alt="Today's special: {{ special.name }}">
+            <h3>{{ special.name }}</h3>
+            <p>{{ special.description }}</p>
+            <p>Price: ${{ special.price }}</p>
         </div>
-        <hr>
-    {% empty %}
-        <p>No menu item available.</p>
     {% endfor %}
+        <h2>Popular Menu Items</h2>
+    {% for item in menu_items %}
 
-    <!-- Pagination Controls -->
-    <div class="pagination">
-        {% if page_obj.has_previous %}
-            <a href="?page=1">Frist</a>
-            <a herf="?page={{ page-obj.previous_page_number }}">Previous</a>
-        {% endif %}
-
-        <span class="current">Page {{ page_obj.number }}of {{ page_obj.paginator.num-pages }}</span
-            
-        {% if page_obj.has_next %}
-            <a herf="?page={{ page_obj.next_page_number }}">Next</a>
-            <a herf="?page={{ page_obj.paginator.num_pages }}">Last</a>
-        {% endif %}
-    </div>
+        <div>
+            <img src="{{ item.image.url }}" alt="Delicious {{ item.name }} served at our restaurant">
+            <h3>{{ item.name }}</h3>
+            <p>{{ item.description }}</p>
+            <p>${{ item.price }}</p>
+        </div>
+    {% endfor %}
 </body>
 </html>
 """
+#about.html
+about_template ="""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us </title>
+</head>
+<body>
+    <h1>About {{ restaurant.name }}</h1>
+    <img src="{{ restaurant.image.url }}" alt="Photo of {{ restaurant.name }}">
+    <p>{{ restaurant.description }}</p>
+</body>
+</html>
+"""
+#menu.html
+menu_template = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Menu</title>
+</head>
+<body>
+    <h1>Our Menu</h1>
+    {% for item in items %}
+        <div>
+            <img src="{{ item.image.url }}" ait="Menu item: {{ item.name }}">
+            <h3>{{ item.name }}</h3>
+            <p>{{ item.description }}</p>
+            <p>${{ item.price }}</p>
+        </div>
+    {% endfor %}
+</body>
+</html>
+"""
+
