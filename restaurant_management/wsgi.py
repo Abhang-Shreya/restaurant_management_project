475f8ddba1d1c2dcd468d602ab4256ddd848b632
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-25 14:12:54 +0530

    38387

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -1,133 +1,100 @@
-#view.py
-from django import forms
-from django.shortcuts import render, redirect
-from django.db import models
-from django.urls import path 
-from .view import contact_view, Thank_you_view
+# view_and_templates.py
+from django.shortcuts import render
+from django.htpp import Httprespone 
+from django.utils import timezone
+from django.urls import path
+from django.conf import settings
+from djanngo.conf.urls.static import static
 
-#Model
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    message = models.TextField()
+#VIEWS
+def home(request):
+    return render(request, "home.html")
 
-    def __str__(self):
-        return self.name
-
-#Form
-class ContactForm(forms.Form):
-    class Meta:
-        model = Contact
-        fields = ['name', 'email', 'message' ]
-
-#View
-def contact_view(request):
-    if request.method =="POST"
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("thank_you") #Redirect after sucess
-    else:
-        form = ContactForm()
-    return render(request, "contact.html", {"form": form})
-
-def thank_you_view(request):
-    return render(request, "thank_you.html")
-
-#Templates 
-#contact.html
-CONTACT_TEMPLATE = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Contact Us</title>
-    <style>
-        body{
-            font-family: Arial, sans-serif;
-            margin: 40px;
-        }
-    
-        form {
-            max-width: 400px;
-            margin: auto;
-        }
-       
-        input, textarea {
-            width: 100;
-            padding: 10px;
-            margin: 30px;
-        }
-
-        button {
-            margin-top: 15px;
-            padding: 10px 20px;
-            background: #28a745;
-            color:white;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-
-    </style>
-</head>
-<body>
-    <div class="container">
-        <h1>Contact Us<h1>
-            
-            <div class="address">
-                <h3>Our Address</h3>
-                <p>{{ restaurant.address }}</p>
-            </div>
-
-            <form method="post">
-                {% csrf_token %}
-                {{ form.as_p }}
-                <button type="submit">Send</button>
-            </form>
-        </div>
-</body>
-</html>
-"""
-
-#template: thank_you.html
-"""
-<!DOCTYPE html>
-<html>
-<head>
-    <titleThank You></title>
-    <style>
-        body{
-            font-family: Arial, sans-serif; 
-            margin: 50px;
-            text-align: Center;
-        }
-        .box {
-            backgroundd: #dff0d8;
-            color: #3c7638d;
-            padding: 20px;
-            border-radius: 10px;
-            display: inline-block;
-        }
-        a{
-            display: block;
-            margin-top: 15px;
-            text-decoration: none; 
-            color: #4CAF50;
-        }
-    </style>
-</head>
-<body>
-    <div class="box">
-        <h2>Thank you</h2>
-        <p>Your message has been sent successfully.</p>
-        <a href="{% url 'contact' %}">Back to Contact Page</a>
-    </div>
-</body>
-</html>
-"""
+def about(request):
+    return render(request, "about.html")
 
 #URLS
-urlpatterns = [
-    path("contact/", contact_view, name="contact"),
-    path("thank-you/" thank_you_view, name="thank_you"),
-]
+urlpatterns =[
+    path('', home, name='home'),    
+    path('about/', about, name='about')
+]+ static(setting.static_URL, document_root=setting.STATIC_ROOT)
+
+#BASE TEMPLATES (with folder)
+# template/base.html
+"""
+<!DOCTYPE html>
+<html>
+<head>
+    <tile>{% block title %}My Website{% endblock %}</title>
+    <style>
+        body{
+
+            font-family:arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background: #333; 
+            color:#fff;
+            padding:10px;
+            text-align: center;
+        }
+        
+        main {
+            min-hegiht: 79vh;
+            padding: 20px;
+        }
+        footer {
+            backgroud: #f2f2f2;
+            text-algin: center;
+            padding: 10px;
+            margin-top:20px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>My Restaurant</h1>
+        <nav>
+            <a herf="/">Home</a> | <a herf="/about/">About</a>
+        </nav>
+    </header>
+
+    <main>
+        {% block contect %}{% endblock %}
+    </main>
+
+    <footer>
+        &copy; {{ year }}My Restaurant. All right reserved.
+    </footer>
+</body>
+<html>
+"""
+
+#ABOUT TEMPLATE
+#templates/about.html
+"""
+{% extend 'base.html' %}
+{% block title %}Home{% endlock %}
+{%block contect %}
+    <h2>Welcome to our restaurant!</h2>
+    <p>This is homepage.</p>
+{% endblock %}
+"""
+
+#ABOUT TEMPLATE
+# template/about.html
+"""
+{% extend 'base.html' %}
+{% block title %}About {% endblock %}
+{% block contect %}
+    <h2><About Us </h2>
+    <p> WE serve delicious food made with love.</p>
+{% endblock %}
+"""
+
+#CONTEXT PROCESSOR(for current year)
+# context_processors.py
+def current_year(request):
+    return {"year": timezone.now().year}
