2646991b0c07524244f4e74dba822b80ed004a60
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-27 23:55:17 +0530

    39841

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -1,81 +1,42 @@
 # staff_project.py
 from django.shortcuts import render
+from django.url import path
+from . import views
 
-def about(request):
-    return render(request, "about.html")
+def contact(request):
+    opening_hour = {
+        "Monday - Friday": "10:00 AM - 10:00 PM",
+        "Saturday": "11:00AM - 11:00PM"
+        "Sunday": "Closed",
+    }
+    context = {
+        "email":"contact@resaturant.com",
+        "opening_hours": opening_hours
+    }
+    return render(request, "contact.html", context)
+    
+
     """
+    <!--templates/contact.html>
     <!DOCTYPE html>
-    <html lang="en">
+    <html>
     <head>
-        <meta lang="en">
-        <title>About Us - Delight Restaurant</title>
-    <style>
-        body{
-            font-family: Arial, sans-serif;
-            line-heght: 1.6;
-            margin: o;
-            padding: 0;
-            background: #f9f9f9;
-            color: #333;
-        }
-        header{
-            background: #8B0000;
-            color: #fff;
-            padding: 20px;
-            text-align: center;
-        }
-        .container{
-            width:80px;
-            margin: 30px auto;
-            background: #fff;
-            padding: 30px;
-            border-radius= 10px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.1);
-        }
-        h1 h2 {
-            color: #8B0000;
-        }
-        p{
-            margin-bottom: 15px;
-        }
-        .section{
-            margin-bottom: 40px;
-        }
-    </style>
+        <title>Contact Us -  Restaurant</title>
 </head>
 <body>
-    <header>
-    <h1>About Us</h1>
-    </header>
+    <h1>Contact Us</h1>
+    <p>Email: <a herf="mailto:{{ email }}>{{ email }}</a></p>
 
-    <div class="container">
-        <div class="section">
-            <h2>Our story</h2>
-            <p>
-                Established in 2010,  </strong>Delight Restaurant</strong> began as a small family-owned eatery
-                with a passion for crafting authentic flavour. What started as a cozy corner spot
-                has now grown into a beloved destination for food lovers, knows for its warn hospitality
-                and carfully prepared dishes. 
-            </p>
-        </div>
-
-        <div class="section">
-            <h2>Our Mission</h2>
-            <p>
-                Our mission is simple: <em>to bring pepole together food</em>.we believe
-                every meal should be an experience of joy, comfort, and communtiy. That's why we focus
-                on using fresh ingredients, sustainable practices, and recipes passed down through gererations.
-            </p>
-        </div>
-
-        <div class="section">
-            <h2>Our Values</h2>
-            <p><strong>Qualitiy: </strong> Only the finest ingredients make it to your plate.</p>
-            <p><strong>Hospitaliy:</strong> Every guest ingredients make it to your plate.</p>
-            <p><strong>Sustainability:</strong>we strive to support local famers and redues waste.</p>
-            <p><strong>Innovation:</strong>While we cherish tradition, we love adding creative twist to classics</p>
-        </div>
-    </div>
+    <h2>opening Hours</h2>
+    <ul>
+        {% for day, hours in opening_hours.items %}
+            <li><strong>{{ day }}</strong> {{ hours }}</li>
+        {% endfor %}
 </body>
 </html>
 """
+
+#URL 
+urlpatterns = [
+    path("contact/", views.contact, name="contact"),
+]
