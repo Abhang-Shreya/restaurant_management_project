c830a4b2f6031377f028c2bfcb9200ce0ed9853f
Abhang-Shreya <abhangshreya2004@gmail.com>
2025-08-28 15:16:16 +0530

    39912

--- restaurant_management/wsgi.py
+++ restaurant_management/wsgi.py
@@ -2,78 +2,69 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Contact us </title>
+    <meta name="viewport" contact="width=device-width, inital-scale=1.0">
+    <title>Restaurant Website </title>
     <style>
         body {
             font-family: Arial, sans-serif;
             margin:0;
             padding:0;
-            background:#fafafa;
-            color: #333;
         }
-        .contact-container {
+
+        header {
             background: #333;
             color:white;
             padding:15px;
             text-align:center;
         }
-        h2{
-            background: #333;
+
+        main{
+            min-heigth: 70px;
             padding: 10px;
             text-algin: center;
         }
-        form {
+
+        footer {
+            backgroud: #222;
             color: #fff;
-            margin: 0 10px;
-            text decoration:none;
+            text-align : center;
+            padding: 1.5em;
         }
-        label{
-            padding:20px;
-            min-heght: 70vh;
+
+        footer p {
+            margin: 0.5em 0;
         }
-        input, textarea {
-            background:#fafafa; 
-            padding:1em;
-            text-algin:center;
-            margin-top:2em;
+
+        footer a  {
+            bcolor: #ffcc00;
+            text-decoration: none;
+            margin-top: 0 0.5em;
         }
-        textarea {
+
+        footer a:hover  {
             text-decoration:underline; 
-            color:#333;
-        }
-        button {
-            background-color: #007BFF;
-            color: White;
-            padding: 12px;
-            font-size: 16px;
-            border: none;
-            border-radius: 6px;
-            cursor: pointer;
-            transition: background 0.3s;
-        }
-        button:hover {
-            display: flex;
-            flex-direction: column;
         }
     </style>
 </head>
 <body>
-    <div class="contact-container">
-    <h2>Contact Us</h2>
-    <form>
-        <div class="form-group">
-            <label for="name">Your Name</label>
-            <input type="text" id="name" placeholder="Enter your name" required>
-        </div>
+    <header>
+        <h1>Welcome to Restaurant</h1>
+    </header>
 
-        <div class="form-group">
-            <label for="email">Email Address</label>
-            <input type="email" id="email" placeholder="Enter your email" requrired>
-        </div>
+    <main>
+        <h2>Delicious meals, crafted with care <h2>
+        <p>Explore our menu and enjoy an unforgettable dining experience. </p>
+    </main>
 
-        <div class="form-group">
-            <label for="message">Message</label>
-            <textarea id="message" placeholder="Write your message" required></textarea>
-        </div>
+    <footer>
+        <p>&copy; 2025 Your Restaurant Name</p>
+        <p>Opening Hour: Mon-Sun: 10:00AM - !0:00Pm</p>
+        <p>
+            <a href="#">Facebook</a>|
+            <a href="#">Instagram</a>|
+            <a href="#">Contact</a>|
+            <a href="#">Term of Service</a>
+        </p>
+    </footer>
 </body>
 </html>
